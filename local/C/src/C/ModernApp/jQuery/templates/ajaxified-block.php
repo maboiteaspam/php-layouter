e5bfc31fd785e630fae0d089143eca3a2a17bdf2
clement <maboiteaspam@wanadoo.fr>
2015-10-05 22:40:29 +0800

    ensure correct html parsing even when debug isoff

--- local/C/src/C/ModernApp/jQuery/templates/ajaxified-block.php
+++ local/C/src/C/ModernApp/jQuery/templates/ajaxified-block.php
@@ -7,8 +7,13 @@
 <script type="text/javascript">
     $.get('<?php echo $url; ?>?target=<?php echo $target; ?>', function(data){
         var receiver = $('#<?php echo $id; ?>');
-        if ($(data).length) {
+        try{
             data = $(data);
+        }catch(ex){
+            data = $("<div>"+data+"</div>").children();
+            console.log(data.html())
+        }
+        if (data.length) {
             if (data.first().is("c_block_node")) {
                 if (data.children().length) {
                     data = data.children().unwrap();
